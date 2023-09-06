7b798945ddf5f0e3f4a227ebdbe03bd0a4265e4e
Megan <mdetels1@jh.edu>
2023-09-06 18:06:11 -0400

    Add answer for exercise 2

--- day2-homework/plot-sisA.py
+++ day2-homework/plot-sisA.py
@@ -3,8 +3,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-transcripts = np.loadtxt( "all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
-print( "transcripts: ", transcripts[0:5] )
+f = open("all_annotated.csv", "r")
+transcripts = f.readlines()
+
+trans=[]
+for i in transcripts[1:]:
+    i = i.rstrip()
+    transcripts_list = i.split(",")
+    i = transcripts_list[0]
+    trans.append(i)
+
+print(trans[0])
+
+#samples = transcripts[0].rstrip().split(",") [2:]
+#print(samples)
+ 
+
+#print(transcripts[0])
+#transcripts = np.loadtxt( "all_annotated.csv", delimiter=",", usecols=0, dtype="<U30", skiprows=1 )
+#print( "transcripts: ", transcripts[0:5] )
 
 samples = np.loadtxt( "all_annotated.csv", delimiter=",", max_rows=1, dtype="<U30" )[2:]
 print( "samples: ", samples[0:5] )
@@ -12,12 +29,13 @@ print( "samples: ", samples[0:5] )
 data = np.loadtxt( "all_annotated.csv", delimiter=",", dtype=np.float32, skiprows=1, usecols=range(2, len(samples) + 2) )
 print( "data: ", data[0:5, 0:5] )
 
-# Find row with transcript of interest
+#Find row with transcript of interest
 for i in range(len(transcripts)):
-    if transcripts[i] == 'FBtr0073461':
+    print(transcripts[i])
+    if 'FBtr0073461' in transcripts[i] :
         row = i
 
-# Find columns with samples of interest
+ #Find columns with samples of interest
 colsf = []
 colsm = []
 for i in range(len(samples)):
@@ -27,11 +45,11 @@ for i in range(len(samples)):
         colsm.append(i)
 
 
-# Subset data of interest
+ #Subset data of interest
 expressionf = data[row, colsf]
 expressionm = data[row,colsm]
 
-# Prepare data
+ #Prepare data
 x = samples[colsf]
 yf = expressionf
 ym = expressionm
@@ -39,7 +57,7 @@ ym2 = 2 * np.array(ym)
 
 x = [10, 11, 12, 13, "14A", "14B", "14C", "14D"]
 
-# Plot data
+ #Plot data
 fig, ax = plt.subplots()
 ax.set_xlabel("developmental stage")
 ax.set_ylabel("mRNA abundance (RPKM)")
