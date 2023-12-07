a08a1f9a73284f04e14a65eedd1a8bd44a7e4475
Megan <mdetels1@jh.edu>
2023-12-07 18:41:30 -0500

    Plot2 code

--- week10-homework/exercise2_plot.py
+++ week10-homework/exercise2_plot.py
@@ -8,30 +8,44 @@ from matplotlib import pyplot as plt
 # read in data
 numbats_df = pd.read_csv("numbats.csv")
 
-# read in australia data
-au = pd.read_csv("au.csv", header = None)
+# # read in australia data
+# au = pd.read_csv("au.csv", header = None)
 
-# Find latitude
-coords = numbats_df.loc[:, ("decimalLatitude", "decimalLongitude")]
+# # Find latitude
+# coords = numbats_df.loc[:, ("decimalLatitude", "decimalLongitude")]
 
-notNaRows = coords.loc[:, "decimalLatitude"].notnull()
-notNaRows2 = coords.loc[:, "decimalLongitude"].notnull()
+# notNaRows = coords.loc[:, "decimalLatitude"].notnull()
+# notNaRows2 = coords.loc[:, "decimalLongitude"].notnull()
 
-coords_noNA = coords.loc[notNaRows & notNaRows2, :]
+# coords_noNA = coords.loc[notNaRows & notNaRows2, :]
 
-# plot australia
+# # plot australia
+# plt.figure()
+# plt.scatter(au.iloc[:,1], au.iloc[:,0], s=1)
+# plt.scatter(coords_noNA["decimalLongitude"], coords_noNA["decimalLatitude"], color="orange", label="Numbat Locations", s=4)
+# plt.legend()
+# plt.title("Locations of Numbats in Australia")
+# plt.xlabel("Longitude")
+# plt.ylabel("Latitude")
+# plt.savefig("Locations of Numbats in Australia.png")
+
+
+# # time of day numbats are seen
+# find time of day
+hour = numbats_df["hour"]
+
+noNahour = hour[hour.notnull()]
+
 plt.figure()
-plt.scatter(au.iloc[:,1], au.iloc[:,0], s=1)
-plt.scatter(coords_noNA["decimalLongitude"], coords_noNA["decimalLatitude"], color="orange", label="Numbat Locations", s=4)
-plt.legend()
-plt.title("Locations of Numbats in Australia")
-plt.xlabel("Longitude")
-plt.ylabel("Latitude")
-plt.savefig("Locations of Numbats in Australia.png")
+plt.hist(noNahour, bins = 24)
+plt.xlabel("Hour of the Day")
+plt.ylabel("Count")
+plt.title("Sightings of Numbats over the Hours of the Day")
+plt.savefig("Numbatsovertheday.png")
 
-# plot on a scatter plot
 
-# find the earth latitude and longitude
+
+
 
 
 
