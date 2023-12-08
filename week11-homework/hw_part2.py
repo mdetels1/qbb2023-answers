65077c97c6fcbf122b3d85343404b12cff24fe36
Megan <mdetels1@jh.edu>
2023-12-08 13:22:34 -0500

    Exercise 3.3

--- week11-homework/hw_part2.py
+++ week11-homework/hw_part2.py
@@ -15,5 +15,13 @@ fig, ax = plt.subplots()
 sc.pl.umap(adata, color=["CD79A","FCER1A","GNLY"], title=["CD79A Expression", "FCER1A Expression", "GNLY Expression"], show=False)
 plt.savefig("Exercise3plot.png")
 
+# CD79A - B cell - cluster 2
+# GNLY - NK cells - cluster 5
+# FCER1A - Dendritic - cluster 6
 
+adata.rename_categories('leiden', ["0", "1", "B Cells", "3", "4", "NK Cells", "Dendritic Cells", "7"])
+
+sc.pl.umap(adata, color="leiden", title="UMAP with New Variable Names", show=False)
+plt.tight_layout()
+plt.savefig("Exercise3plotpart2.png")
 
